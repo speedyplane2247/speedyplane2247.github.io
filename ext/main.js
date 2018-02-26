bbd03a7a605ad2ef409ab6deb1332d552542bac6
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2018-02-26 10:54:24 -0500

    Create main.js

---
+++ ext/main.js
@@ -0,0 +1,18 @@
+/*
+jsCoder
+(c) speedyplane 2247
+Client Backend, under MIT license.
+*/
+function encode() {
+var decodedURI = document.getElementById("decode");
+var decodedURI = input; // input comes 
+var encodedURI = encodeURI(decodedURI);
+document.getElementByID("outputEncoded").innerHTML = "Encoded URI: " + encodedURI
+}
+function decode() {
+  // tbd
+}
+
+/*
+Continue
+*/
