d37642ac9fa45b7941add8d6eb0479b5f5e232c9
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2018-02-26 11:01:57 -0500

    Update main.js

--- ext/main.js
+++ ext/main.js
@@ -4,9 +4,15 @@ jsCoder
 Client Backend, under MIT license.
 */
 function encode() {
-var decodedURI = document.getElementById("decode");
+  try {
+    var decodedURI = document.getElementById("decode");
 var encodedURI = encodeURI(decodedURI);
 document.getElementByID("outputEncoded").innerHTML = "Encoded URI: " + encodedURI
+}
+catch(err) {
+    alert(err.message)
+}
+
 }
 function decode() {
   // tbd
