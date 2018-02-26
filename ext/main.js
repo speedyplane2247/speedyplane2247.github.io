b9e222b20ac1a04804af2e7a82df45518914a1da
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2018-02-26 10:55:13 -0500

    Update main.js

--- ext/main.js
+++ ext/main.js
@@ -5,7 +5,6 @@ Client Backend, under MIT license.
 */
 function encode() {
 var decodedURI = document.getElementById("decode");
-var decodedURI = input; // input comes 
 var encodedURI = encodeURI(decodedURI);
 document.getElementByID("outputEncoded").innerHTML = "Encoded URI: " + encodedURI
 }
